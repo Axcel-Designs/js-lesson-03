f6018994563a318e9c1f00c34c6479f186f6f034
excel <axceldesigns@gmail.com>
2024-08-31 09:50:57 +0100

    lesson 03

--- task08.js
+++ task08.js
@@ -1,9 +1,9 @@
 // Task 8: Use nested loops
 
-for (x =1; x <= 12; x++){
+for (x =1; x <= 13; x++){
     for(y=1; y <= 12; y++){
         let z = x*y;
         console.log(`${x} x ${y} = ${z}\t`);
     }
-    // console.log();
+    console.log();
 }
--- task10.js
+++ task10.js
@@ -1,7 +1,11 @@
 // Task 10: Use "for...of" loop for iterable objects
 
 const  word = 'string'
+const array = [12,23,34,45,56,67,78,89];
 
 for (x of word){
     console.log(x);
+}
+for (y of array){
+    console.log(y);
 }

0c4de0268a651e16149d74a4676c864a1303ec19
excel <axceldesigns@gmail.com>
2024-09-12 05:31:58 +0100

    lesson 03

--- task09.js
+++ task09.js
@@ -1,6 +1,6 @@
 // Task 9: Use array methods for iteration
 
-const names =['lola', 'glory', 'matthew', 'grace']
+const names =['lola', 'glory', 'matthew', 'grace', 'ola']
 
 names.forEach(function(name) {
     console.log("Name:", name);
